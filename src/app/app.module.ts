e439d1e5bdc868f54e4b5a78242fb3432836a046
Speros Misirlakis <smisirlakis@gmail.com>
2017-06-16 15:35:27 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
